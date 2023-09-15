149267c35dae76255c7e2876ef523dabde86e5cc
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-09-15 16:54:52 -0400

    inital commit of splash screen

---
+++ src/components/splashscreen.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Image from 'next/image'; 
+
+const SplashScreen: React.FC = () => {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: 'url(/DSCF6354.jpg)' }}>
+        <div className="w-32 mx-auto mb-4 logo">
+            <Image src={'/trapiche-logo2.png'} alt="Logo" layout="responsive" width={12} height={12} />
+        </div>
+        <div className="text-center" style={{ transform: 'translateY(-333%)' }}>
+            <h1 className="text-4xl font-bold text-white">Coming Soon</h1>
+        </div>
+    </div>
+  );
+};
+
+export default SplashScreen;

9b9ab9da7719134304fe7ae1bf0d94517668fdb1
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-09-16 14:50:23 -0400

    adding opengraph and twitter metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,33 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
+  metadataBase: new URL('https://PanaderiaTrapiche.com'),
+  icons: {
+      icon: '/favicon.ico'
+  },
   title: 'Panaderia Trapiche',
   description: 'Retiro desde el coche',
+  alternates: {
+      canonical: '/',
+      languages: {
+          'en-US': '/en-US',
+          'es-US': '/es-US'
+      }
+  },
+  openGraph: {
+      siteName: 'PanaderiaTrapiche.com',
+      description: 'Retiro desde el coche.',
+      title: 'Panaderia Trapiche',
+      locale: 'en-US',
+      images: '/trapiche-logo2.png',
+      videos: 'https://www.youtube.com/watch?v=ti2ob5111g8'
+  },
+  twitter: {
+      card: 'summary_large_image',
+      title: 'Panaderia Trapiche',
+      images: '/trapiche-logo2.png',
+      description: 'Retiro desde el coche.',
+  }
 }
 
 export default function RootLayout({

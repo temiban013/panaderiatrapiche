dab29ce5749cf81f024c220acad27c7caf76173d
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-09-25 19:37:02 -0400

    added a different logo for comparison

--- src/components/splashscreen.tsx
+++ src/components/splashscreen.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import Image from 'next/image'; 
-import Head from 'next/head';
+'use client'
+
+import React, {useState} from 'react'
+import Image from 'next/image'
+import Head from 'next/head'
 
 const SplashScreen: React.FC = () => {
+  const [logo, setLogo] = useState('/panaderia-trapiche-logo.gif')
+  const alternateLogo = () => {
+      setLogo(logo === '/panaderia-trapiche-logo.gif' ? '/panaderia-trapiche-logo2.gif' : '/panaderia-trapiche-logo.gif')
+  }
   return (
     <>
       <Head>
@@ -11,7 +17,7 @@ const SplashScreen: React.FC = () => {
       </Head>
       <div className="flex items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: 'url(/DSCF6354.jpg)' }}>
           <div className="w-32 mx-auto mb-4 logo">
-              <Image src={'/panaderia-trapiche-logo.gif'} alt="Logo" layout="responsive" width={12} height={12} />
+              <Image src={logo} alt="Logo" layout="responsive" width={12} height={12} onClick={alternateLogo}/>
           </div>
           <div className="text-center" style={{ transform: 'translateY(-333%)' }}>
               <h1 className="text-4xl font-bold text-white">Coming Soon</h1>

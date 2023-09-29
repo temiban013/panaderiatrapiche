0eec1f4897463343dd46e21649bf048b9701095b
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-09-29 14:22:16 -0400

    Adding an extra logo

--- src/components/splashscreen.tsx
+++ src/components/splashscreen.tsx
@@ -5,10 +5,13 @@ import Image from 'next/image'
 import Head from 'next/head'
 
 const SplashScreen: React.FC = () => {
-  const [logo, setLogo] = useState('/panaderia-trapiche-logo.gif')
+  const logos = ['/panaderia-trapiche-logo1.gif', '/panaderia-trapiche-logo2.gif', '/panaderia-trapiche-logo3.gif'];
+  const [logoIndex, setLogoIndex] = useState(0);
+  
   const alternateLogo = () => {
-      setLogo(logo === '/panaderia-trapiche-logo.gif' ? '/panaderia-trapiche-logo2.gif' : '/panaderia-trapiche-logo.gif')
+    setLogoIndex((logoIndex + 1) % logos.length);
   }
+
   return (
     <>
       <Head>
@@ -16,8 +19,8 @@ const SplashScreen: React.FC = () => {
         <meta name='deacription' content='Retiro desde el coche' />
       </Head>
       <div className="flex items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: 'url(/DSCF6354.jpg)' }}>
-          <div className="w-32 mx-auto mb-4 logo">
-              <Image src={logo} alt="Logo" layout="responsive" width={12} height={12} onClick={alternateLogo}/>
+          <div className="w-32 mx-auto mb-4 logo rounded">
+              <Image src={logos[logoIndex]} alt="Logo" layout="responsive" width={12} height={12} onClick={alternateLogo}/>
           </div>
           <div className="text-center" style={{ transform: 'translateY(-333%)' }}>
               <h1 className="text-4xl font-bold text-white">Coming Soon</h1>

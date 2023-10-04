00096dbec64ce6b2b475c3341b05a4a13af91856
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-10-04 16:31:36 -0400

    Changing background splash image

--- src/components/splashscreen.tsx
+++ src/components/splashscreen.tsx
@@ -18,7 +18,7 @@ const SplashScreen: React.FC = () => {
         <title>Panaderia Trapiche</title>
         <meta name='deacription' content='Retiro desde el coche' />
       </Head>
-      <div className="flex items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: 'url(/DSCF6354.jpg)' }}>
+      <div className="flex items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: 'url(https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694794015/Trapiche/gtm8rxob7s6aixhnqmef.jpg)' }}>
           <div className="w-32 mx-auto mb-4 logo rounded">
               <Image src={logos[logoIndex]} alt="Logo" layout="responsive" width={12} height={12} onClick={alternateLogo}/>
           </div>

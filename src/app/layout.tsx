a83141df6aed4130158df687c4817e70d2e3ca59
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-10-04 16:25:14 -0400

    Adding Cloudinary functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,13 +23,13 @@ export const metadata: Metadata = {
       description: 'Retiro desde el coche.',
       title: 'Panaderia Trapiche',
       locale: 'en-US',
-      images: '/panaderia-trapiche-logo.png',
+      images: 'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694794015/Trapiche/gtm8rxob7s6aixhnqmef.jpg',
       //videos: 'https://www.youtube.com/watch?v=ti2ob5111g8'
   },
   twitter: {
       card: 'summary_large_image',
       title: 'Panaderia Trapiche',
-      images: '/panaderia-trapiche-logo.png',
+      images: 'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694794015/Trapiche/gtm8rxob7s6aixhnqmef.jpg',
       description: 'Retiro desde el coche.',
   }
 }

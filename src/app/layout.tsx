95d28d73d315e04092b91d644b2c8503c8d2f57f
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-11-07 08:13:22 -0400

    ico update / metadata update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,19 +10,22 @@ export const metadata: Metadata = {
       icon: '/favicon.ico'
   },
   title: 'Panaderia Trapiche | San German',
+  keywords: 'panaderia, galleta de manteca, budin de pan, pan artesanal, dulces, sandwiches, comidas criollas, san german, puerto rico, bakery, bread, artisan bread, sweets, sandwiches, creole food, pizzas, ensaladas, frituras, platos principales, postres',
   description: 'Panadería Trapiche es conocida por su delicioso pan de manteca y budín de pan. Ofrecemos una amplia variedad de panes artesanales, dulces, sándwiches y comidas criollas. Nuestro menú incluye opciones de café y cappuccino, más de diez tipos de panes artesanales, pizzas con opciones de carne y vegetales, ensaladas, sándwiches, wraps, frituras y un menú variado de platos principales y postres. Visítanos en nuestro local ampliado con serví carro y disfruta de la panadería favorita en San Germán y pueblos limítrofes.',
   alternates: {
       canonical: '/',
       languages: {
           'en-US': '/en-US',
-          'es-US': '/es-US'
+          'es-PR': '/es-PR'
       }
   },
   openGraph: {
-      siteName: 'PanaderiaTrapiche.com',
+      siteName: 'Panaderia Trapiche',
+      url: 'https://PanaderiaTrapiche.com',
+      type: 'website',
       title: 'Panaderia Trapiche | San German',
       description: 'Panadería Trapiche es conocida por su delicioso pan de manteca y budín de pan. Ofrecemos una amplia variedad de panes artesanales, dulces, sándwiches y comidas criollas. Nuestro menú incluye opciones de café y cappuccino, más de diez tipos de panes artesanales, pizzas con opciones de carne y vegetales, ensaladas, sándwiches, wraps, frituras y un menú variado de platos principales y postres. Visítanos en nuestro local ampliado con serví carro y disfruta de la panadería favorita en San Germán y pueblos limítrofes.',
-      locale: 'en-US',
+      locale: 'es-PR',
       images: 'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694794015/Trapiche/gtm8rxob7s6aixhnqmef.jpg',
       //videos: 'https://www.youtube.com/watch?v=ti2ob5111g8'
   },
--- src/app/menu/layout.tsx
+++ src/app/menu/layout.tsx
@@ -10,19 +10,22 @@ export const metadata: Metadata = {
       icon: '/favicon.ico'
   },
   title: 'Panaderia Trapiche | Menu',
+  keywords: 'panaderia, galleta de manteca, budin de pan, pan artesanal, dulces, sandwiches, comidas criollas, san german, puerto rico, bakery, bread, artisan bread, sweets, sandwiches, creole food, pizzas, ensaladas, frituras, platos principales, postres',
   description: 'Descubre un menú variado en Panadería Trapiche: café y cappuccino, más de diez tipos de panes artesanales como pan de anón y ajonjolí, pizzas con opciones de carne y vegetales, ensaladas cesar y de pulpo, sandwiches y wraps, frituras y pastelillos de carne, pollo y bacon. Prueba nuestros platos principales como pechuga a la mexicana, almuerzo criollo, lomo de cerdo en salsa de zetas y dorado en salsa criolla. No te pierdas nuestros postres: cheesecake de oreo, chocolate o fresas, brazo gitano, budín y bizcochos.',
   alternates: {
-      canonical: '/',
+      canonical: '/menu',
       languages: {
           'en-US': '/en-US',
-          'es-US': '/es-US'
+          'es-PR': '/es-PR'
       }
   },
   openGraph: {
-      siteName: 'PanaderiaTrapiche.com/menu',
+      siteName: 'Panaderia Trapiche',
+      url: 'https://PanaderiaTrapiche.com/menu',
+      type: 'website',
       description: 'Descubre un menú variado en Panadería Trapiche: café y cappuccino, más de diez tipos de panes artesanales como pan de anón y ajonjolí, pizzas con opciones de carne y vegetales, ensaladas cesar y de pulpo, sandwiches y wraps, frituras y pastelillos de carne, pollo y bacon. Prueba nuestros platos principales como pechuga a la mexicana, almuerzo criollo, lomo de cerdo en salsa de zetas y dorado en salsa criolla. No te pierdas nuestros postres: cheesecake de oreo, chocolate o fresas, brazo gitano, budín y bizcochos.',
       title: 'Panaderia Trapiche | Menu',
-      locale: 'en-US',
+      locale: 'es-PR',
       images: 'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694794030/Trapiche/lsgceibitwu69x9ntulf.jpg',
       //videos: 'https://www.youtube.com/watch?v=ti2ob5111g8'
   },

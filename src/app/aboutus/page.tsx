325c1960da5179806dba96dca2f454ce0488d319
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-11-07 17:18:51 -0400

    Adding logo to About us

--- src/app/aboutus/page.tsx
+++ src/app/aboutus/page.tsx
@@ -9,7 +9,15 @@ export default async function AboutUs() {
         
       <div className="snap-x snap-mandatory snap-align-start snap-justify-start overflow-x-auto">
         
-      <Image className="snap-center shrink-0" alt='Dish 1' src="https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1699376525/Trapiche/Menus/About_US_hvnvcc.jpg" width={2000} height={533} />
+        <Image className="snap-center shrink-0" alt='Dish 1' src="https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1699376525/Trapiche/Menus/About_US_hvnvcc.jpg" width={2000} height={533} />
+        <div className="absolute top-5 right-2 lg:right-32 md:right-16">
+          <div className="absolute top-0 right-2 lg:right-5 w-12 md:w-32 lg:w-36">
+            <Image src="https://res.cloudinary.com/drc0myo7z/image/upload/v1697631966/Trapiche/logos/panaderia-trapiche-logo-transp_svx12v.gif" alt="Logo" layout="responsive" width={12} height={12} />
+          </div>
+          <div className="absolute top-6 lg:top-20 md:top-20 right-0 w-16 md:w-40 lg:w-48">
+            <Image src="https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_256/v1697420759/Trapiche/logos/Logos-Trapiche-Bakery-1-_1__yg7wqm.gif" alt="Logo" layout="responsive" width={12} height={12} />
+          </div>
+        </div>
       </div>
         {/* <div className="flex items-stretch flex-col justify-center">
           <h1>About Us</h1>

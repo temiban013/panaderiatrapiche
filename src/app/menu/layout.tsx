8609cc1d7ed5d608937645cc47a18dbf27ccda53
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-11-07 13:29:08 -0400

    Added nav items to home

--- src/app/menu/layout.tsx
+++ src/app/menu/layout.tsx
@@ -26,13 +26,13 @@ export const metadata: Metadata = {
       description: 'Descubre un menú variado en Panadería Trapiche: café y cappuccino, más de diez tipos de panes artesanales como pan de anón y ajonjolí, pizzas con opciones de carne y vegetales, ensaladas cesar y de pulpo, sandwiches y wraps, frituras y pastelillos de carne, pollo y bacon. Prueba nuestros platos principales como pechuga a la mexicana, almuerzo criollo, lomo de cerdo en salsa de zetas y dorado en salsa criolla. No te pierdas nuestros postres: cheesecake de oreo, chocolate o fresas, brazo gitano, budín y bizcochos.',
       title: 'Panaderia Trapiche | Menu',
       locale: 'es-PR',
-      images: 'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694794030/Trapiche/lsgceibitwu69x9ntulf.jpg',
+      images: 'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694794037/Trapiche/se496dfjzsbl44fgw4j4.jpg',
       //videos: 'https://www.youtube.com/watch?v=ti2ob5111g8'
   },
   twitter: {
       card: 'summary_large_image',
       title: 'Panaderia Trapiche | Menu',
-      images: 'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694794030/Trapiche/lsgceibitwu69x9ntulf.jpg',
+      images: 'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694794037/Trapiche/se496dfjzsbl44fgw4j4.jpg',
       description: 'Descubre un menú variado en Panadería Trapiche: café y cappuccino, más de diez tipos de panes artesanales como pan de anón y ajonjolí, pizzas con opciones de carne y vegetales, ensaladas cesar y de pulpo, sandwiches y wraps, frituras y pastelillos de carne, pollo y bacon. Prueba nuestros platos principales como pechuga a la mexicana, almuerzo criollo, lomo de cerdo en salsa de zetas y dorado en salsa criolla. No te pierdas nuestros postres: cheesecake de oreo, chocolate o fresas, brazo gitano, budín y bizcochos.',
   }
 }
--- src/components/splashscreen.tsx
+++ src/components/splashscreen.tsx
@@ -1,38 +1,23 @@
 'use client'
 
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function SplashScreen () {
   const logos = ['/panaderia-trapiche-logo1.gif', '/panaderia-trapiche-logo2.gif', '/panaderia-trapiche-logo3.gif']
-  const backgrounds = [
-    'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694130974/Trapiche/trapiche11_myahqg.jpg',
-    'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694793735/Trapiche/pfhxdjyfbwzixsd9bmxu.jpg',
-    'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694794037/Trapiche/se496dfjzsbl44fgw4j4.jpg',
-    'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694793739/Trapiche/uimnbieqdnje89fuf6ny.jpg',
-    'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694130977/Trapiche/trapiche08_rsxx64.jpg',
-    'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694793702/Trapiche/dtvhcrr0b1yihmeg3w4o.jpg',
-    'https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694794020/Trapiche/owsqqfgdnlrvfvkem6iw.jpg',
-  ]
+
   const [logoIndex, setLogoIndex] = useState(0)
-  const [backgroundIndex, setBackgroundIndex] = useState(0)
   
   const alternateLogo = () => {
     setLogoIndex((logoIndex + 1) % logos.length)
   }
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setBackgroundIndex((backgroundIndex + 1) % backgrounds.length)
-    }, 15000); // Change the interval time (in milliseconds) as needed
-    return () => clearInterval(interval)
-  }, [backgroundIndex, backgrounds.length])
-
   return (
     <>
       <div className="flex flex-col min-h-screen bg-cover bg-center justify-start pt-28" 
         style={{ backgroundImage: `url(https://res.cloudinary.com/drc0myo7z/image/upload/v1694794032/Trapiche/csfog9quttnwc4fslclk.jpg)` }}>
-          <div className='flex items-center justify-center sm:justify-end sm:pr-28'>  
+          <div className='flex items-center justify-center sm:justify-end sm:pr-28 sm:pb-10'>  
               <div className='absolute ml-0 sm:ml-4 flex pb-24'> 
                   <div className='relative w-40 h-58'>
                       <Image src='https://res.cloudinary.com/drc0myo7z/image/upload/v1697631966/Trapiche/logos/panaderia-trapiche-logo-transp_svx12v.gif' 
@@ -43,6 +28,15 @@ export default function SplashScreen () {
                   <Image src='https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_256/v1697420759/Trapiche/logos/Logos-Trapiche-Bakery-1-_1__yg7wqm.gif' 
                       alt="Logo" layout="responsive" width={12} height={12}/>
               </div>     
+          </div>
+          <div className='text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl font-bold text-white text-center text-shadow-black-outline pt-20 sm:pt-0 p-10'>
+            <Link href="/menu">Menu</Link>
+          </div>
+          <div className='text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl font-bold text-white text-center text-shadow-black-outline p-10'>
+            <Link href="/aboutus">De nosotros</Link>
+          </div>
+          <div className='text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl font-bold text-white text-center text-shadow-black-outline p-10'>
+            <Link href="/contactus">Contactenos</Link>
           </div>
       </div>
     </>

87549744adaf982b275d340983d6b65bb2a0d833
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-11-07 20:00:33 -0400

    Updated splashscreen menu and logo in col

--- src/components/splashscreen.tsx
+++ src/components/splashscreen.tsx
@@ -15,29 +15,27 @@ export default function SplashScreen () {
 
   return (
     <>
-      <div className="flex flex-col min-h-screen bg-cover bg-center justify-start pt-28" 
+      <div className="flex flex-col min-h-screen bg-cover bg-center justify-center items-center" 
         style={{ backgroundImage: `url(https://res.cloudinary.com/drc0myo7z/image/upload/v1694794032/Trapiche/csfog9quttnwc4fslclk.jpg)` }}>
-          <div className='flex items-center justify-center sm:justify-end sm:pr-28 sm:pb-10'>  
-              <div className='absolute ml-0 sm:ml-4 flex pb-24'> 
-                  <div className='relative w-40 h-58'>
-                      <Image src='https://res.cloudinary.com/drc0myo7z/image/upload/v1697631966/Trapiche/logos/panaderia-trapiche-logo-transp_svx12v.gif' 
-                          alt="Logo" layout="responsive" width={12} height={12}/>
-                  </div>
-              </div>    
-              <div className="relative w-48 h-48">
-                  <Image src='https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_256/v1697420759/Trapiche/logos/Logos-Trapiche-Bakery-1-_1__yg7wqm.gif' 
-                      alt="Logo" layout="responsive" width={12} height={12}/>
-              </div>     
+        <div className="-top-2 relative flex flex-col justify-center items-center">
+          <div className="absolute -top-12 w-32 md:w-32">
+            <Image src="https://res.cloudinary.com/drc0myo7z/image/upload/v1697631966/Trapiche/logos/panaderia-trapiche-logo-transp_svx12v.gif" alt="Logo" layout="responsive" width={12} height={12} />
           </div>
-          <div className='text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl font-bold text-white text-center text-shadow-black-outline pt-20 sm:pt-0 p-10'>
+          <div className="absolute top-0 w-44 md:w-44 lg:w-48">
+            <Image src="https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_256/v1697420759/Trapiche/logos/Logos-Trapiche-Bakery-1-_1__yg7wqm.gif" alt="Logo" layout="responsive" width={12} height={12} />
+          </div>
+        </div>
+        <div className='flex flex-col justify-center items-center pt-44'>
+          <div className='text-5xl md:text-6xl lg:text-7xl font-bold text-trapiche-tan text-center text-shadow-black-outline p-10'>
             <Link href="/menu">Menu</Link>
           </div>
-          <div className='text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl font-bold text-white text-center text-shadow-black-outline p-10'>
+          <div className='text-5xl md:text-6xl lg:text-7xl font-bold text-trapiche-tan text-center text-shadow-black-outline p-10'>
             <Link href="/aboutus">De nosotros</Link>
           </div>
-          <div className='text-3xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-5xl font-bold text-white text-center text-shadow-black-outline p-10'>
+          <div className='text-5xl md:text-6xl lg:text-7xl font-bold text-trapiche-tan text-center text-shadow-black-outline p-10'>
             <Link href="/contactus">Contactenos</Link>
           </div>
+        </div>
       </div>
     </>
   )

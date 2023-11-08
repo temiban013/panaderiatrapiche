f31e34c0708c9d2a667c8f5b69528176b8393677
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-11-07 20:30:37 -0400

    Updated Contactus with colors and arrangements

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -2,13 +2,14 @@
 
 export default async function ContactUs() {
   return (
-    <div className="flex sm: flex-col md:flex-row gap-4 p-10">
-      <div className='flex w-1/3 flex-col justify-center'>
-        <h1>Contact Us</h1>
-        <h3>187 Luna Street,</h3>
-        <h3> Edificio Rali 105</h3>
-        <h3>San Germán, 00683</h3>
-        <h3>Puerto Rico.</h3>
+    <div className="flex flex-col bg-cover bg-trapiche-orange min-h-screen p-10">
+      <div className='flex w-full flex-col  justify-center text-center'>
+        <div className="text-4xl text-trapiche-tan pb-5 pt-40">Contact Us</div>
+        <div className="text-xl pb-3">187 Luna Street,</div>
+        <div className="text-xl pb-3"> Edificio Rali 105</div>
+        <div className="text-xl pb-3">San Germán, 00683</div>
+        <div className="text-xl pb-3">Puerto Rico.</div>
+        <div className="text-xl pb-3">+1 787-892-4673</div>
       </div>
     </div>
   )

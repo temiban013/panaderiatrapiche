c30b5873586d9140202148f5b3bd3d3f9b62f3b2
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-11-12 17:39:02 -0400

    Moved globals.css to page vs layout

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -3,6 +3,7 @@
 import Head from 'next/head'
 import LongMenu from '../../components/longmenu'
 import Layout from './layout'
+import './../globals.css'
 
 export default async function Menu() {
   return (

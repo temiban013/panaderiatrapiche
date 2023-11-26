a583bd29683cd4207e002dc0fbacaae0acb2fa61
Mario Rafael Ayala <137118271+temiban013@users.noreply.github.com>
2023-11-26 08:27:51 -0400

    Bug fix on change of address plus metadata

--- src/app/contactus/page.tsx
+++ src/app/contactus/page.tsx
@@ -2,10 +2,21 @@
 
 import Link from "next/link"
 import Image from "next/image"
+import Head from "next/head"
 
 export default async function ContactUs() {
   return (
     <div className="flex flex-col bg-cover bg-trapiche-orange min-h-screen p-14">
+      <Head>
+        <title>Contact Us | Panaderia Trapiche</title>
+        <meta name="description" content="Get in touch with Panaderia Trapiche. Find our location, phone number, and email address." />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="Contact Us | Panaderia Trapiche" />
+        <meta property="og:description" content="Get in touch with Panaderia Trapiche. Find our location, phone number, and email address." />
+        <meta property="og:image" content="https://res.cloudinary.com/drc0myo7z/image/upload/c_scale,w_1080/v1694794024/Trapiche/mhezye1pgxvzmrsuqpqp.jpg" />
+        <meta property="og:url" content="https://panaderiatrapiche.com/contactus" />
+        <meta property="og:type" content="website" />
+      </Head>
       <div className='flex justify-center items-center'>
         <div className='text-xl md:text-4xl lg:text-5xl font-bold text-trapiche-tan p-5'>
           <Link href="/">Inicio</Link>
@@ -27,8 +38,7 @@ export default async function ContactUs() {
       </div>
       <div className='flex w-full flex-col pt-48 justify-center text-center'>
         <div className="text-4xl text-trapiche-tan pb-10 pt-24">Contact Us</div>
-        <a href="https://www.google.com/maps/search/?api=1&query=Panaderia Trapiche,+San+Germán,+00683,+Puerto+Rico" className="sm:inline-block text-xl pb-3">187 Luna Street,</a>
-        <div className="text-xl pb-3"> Ave. Atléticos #191</div>
+        <a href="https://www.google.com/maps/search/?api=1&query=Panaderia+Trapiche,+San+Germán,+00683,+Puerto+Rico" className="sm:inline-block text-xl pb-3">Ave. Atléticos #191</a>
         <div className="text-xl pb-3">San Germán, 00683</div>
         <div className="text-xl pb-3">Puerto Rico.</div>
         <a href="tel:+17878924673" className="sm:inline-block text-xl pb-3">+1 787-892-4673</a>
